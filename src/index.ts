6da8297f33aa4a6b141db11f009e7828adcdf262
Filip Kozik <536410@mail.muni.cz>
2024-09-29 20:51:53 +0200

    feat: created prisma schema for messages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import cookieParser from 'cookie-parser';
 
 const prisma = new PrismaClient();
 
@@ -7,34 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
-app.post('/users', async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: "test@gmail.com",
-        password: "password",
-        username: "tester",
-        profilePic: "testingPic"
-      },
-    });
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error creating user' });
-  }
-});
-
-app.get("/users", async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error fetching users' });
-  }
-})
+app.use(cookieParser());
 
 // Start the server
 app.listen(port, () => {
