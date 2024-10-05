f124d9a4bc862a1edaafa152ab546e3bc095213d
Filip Kozik <536410@mail.muni.cz>
2024-10-05 17:24:12 +0200

    fix: send array of keys not object

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -8,17 +8,32 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
+const userSocketMap: Map<String, String> = new Map();
+
 io.on("connection", (socket) => {
-  console.log("a user connected,", socket.id)
+  console.log("a user connected,", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (typeof userId === "string") {
+    userSocketMap.set(userId, socket.id);
+  } else {
+    console.warn("User ID is not a valid string:", userId);
+  }
+
+  io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
 
   // socket.on() is used to listen to the events, can be used both on client and server side
   socket.on("disconnect", () => {
-    console.log("user disconnected,", socket.id)
-  })
-})
+    console.log("user disconnected,", socket.id);
+    if (typeof userId === "string") {
+      userSocketMap.delete(userId);
+      io.emit("getOnlineUsers", Array.from(userSocketMap.keys()));
+    }
+  });
+});
 
 export { app, io, server };
